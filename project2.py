7e1efa582d0a86a81109087b5da7517e1f6fe19a
sameer <shinobininja031@gmail.com>
2026-08-14 21:27:17 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,58 @@
+#program to find the number from the user is even or odd.
+
+'''a=int(input("Enter your number:"))
+if(a%2==0):
+    print('Number is Even')
+else:
+    print('Number is Odd')'''
+
+
+
+#program to display whether a number from the user is positive or negative number.
+
+'''a=int(input("Enter your number:"))
+if(a>0):
+    print("number is positive")
+else:
+    print("negative number")'''
+
+
+    
+#program to find out the greatest among three number.
+
+'''a=int(input("Enter your number:"))
+b=int(input("Enter your number:"))
+c=int(input("Enter your number:"))
+if a>b>c:
+    print("a is greater")
+elif b>c>a:
+    print("b is greater")
+else:
+    print("c is greater")'''
+
+
+    
+#program to display grade of the students.
+
+percentage=float(input("Enter the student percentage:"))
+if percentage>90:
+    print("Grade O")
+elif percentage>=80 and percentage<=89:
+    print("Grade A+")
+elif percentage>=70 and percentage<=79:
+    print("Grade A")
+elif percentage>=60 and percentage<=69:
+    print("Grade B+")    
+elif percentage>=50 and percentage<=59:
+    print("Grade B")
+elif percentage>=40 and percentage<=49:
+    print("Grade C")
+elif percentage>=35 and percentage<=39:
+    print("Grade D")
+else:
+    print("Grade F")    
+    
+
+
+
+    
