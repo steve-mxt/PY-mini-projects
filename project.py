f3389973c3e4a6c8f3d727b8d8be5d17b974dbb3
sameer <shinobininja031@gmail.com>
2026-08-14 20:55:56 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,34 @@
+#Arithmetic operators
+
+'''a=int(input("Enter your first number :"))
+b=int(input("Enter your second number :"))
+print("Add :",a+b)
+print("Sub :",a-b)
+print("Multiply :",a*b)
+print("Divide :",a/b)
+print("Modulus :",a%b)'''
+
+\n---------------------------------------------------
+
+#Relational operator
+#this output gives in boolean expression 
+'''a=int(input("Enter your number:"))
+b=int(input("Enter your number:"))
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)'''
+
+\n----------------------------------------------------
+
+#Membership operator
+
+user_day=input("Enter a day of week :")
+weekend=["saturday","sunday"]
+if user_day in weekend:
+     print("yay! it is weekend")
+else:
+    print("back to studies")
+
+\n----------------------------------------------------    
+      
